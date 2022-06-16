b2236a4f1d12f36e7339f3cc238f253fad48b362
Guido Lorenzetti <102630216+GuidoLorenzetti@users.noreply.github.com>
2022-06-16 08:29:51 +0000

    Unified funtions "lat" and "long" in only one funtion "pos"

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import csv
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-def lat(r,l,lat,dist_hor):
-  x = 0
+def pos(r,l,lat,dist_hor):
+  x = -1
   i = r
   while i < l:
     if lat > i and lat < (i - dist_hor):
@@ -12,20 +12,11 @@ def lat(r,l,lat,dist_hor):
     i = i - dist_hor
     x += 1
 
-def long(u,d,long,dist_ver):
-  y = 0
-  j = u
-  while j < d:
-    if long > j and long < (j - dist_ver):
-      return y
-    j = j - dist_ver
-    y += 1
-
 lat_izq = 55.733342
 lat_der = 55.614935
 long_arr = 12.423467
 long_ab = 12.686726
-inter = 7
+inter = 50
 
 dist_hor = (lat_der - lat_izq) / inter
 dist_ver = (long_arr - long_ab) / inter
@@ -33,39 +24,29 @@ dist_ver = (long_arr - long_ab) / inter
 cant = np.zeros((inter, inter))
 precio = np.zeros((inter, inter))
 precio_prom = np.zeros((inter, inter))
-print (cant)
-print (precio)
-print(precio_prom)
 
 with open('listings.csv', newline='') as File:
     reader = csv.reader(File)
     for row in reader:
         if row[0] == "id":
           continue
-        x = lat (lat_der,lat_izq,float(row[6]),dist_hor)
-        y = long (long_arr,long_ab,float(row[7]),dist_ver)
-        cant[(x-1, y-1)] += 1
-        precio[(x-1, y-1)] += float(row[9])
+        x = pos (lat_der,lat_izq,float(row[6]),dist_hor)
+        y = pos (long_arr,long_ab,float(row[7]),dist_ver)
+        cant[(x, y)] += 1
+        precio[(x, y)] += float(row[9])
 
 for l in range(inter):
   for m in range (inter):
     if cant[(l, m)] != 0:
       precio_prom[(l, m)] = precio[(l, m)] / cant[(l, m)]
 
-print (cant)
-print (precio)
-print(precio_prom)
+for l in range(inter):
+      print(precio_prom[(l, 24)])
 
-#plt.imshow(precio_prom, cmap='hot', interpolation='nearest', vmin=0, vmax=2000)
-#plt.show()
-
-#ax = sns.heatmap(precio_prom, yticklabels=False, vmin=0, vmax=2000)
-#plt.show()
 
 figure, axes = plt.subplots()
-precio_prom = axes.pcolormesh(precio_prom, cmap='hot', vmin=0, vmax=2000)
-axes.set_title('Mapa de Calor de Copenhagen')
+precio_prom = axes.pcolormesh(precio_prom, cmap='inferno', vmin=0, vmax=2000)
+axes.set_title('Copenhagen Heat Map')
 figure.colorbar(precio_prom)
 #inferno
-
 plt.show()

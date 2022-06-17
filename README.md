67f1da503168d96cbf8c1b973adcea5a76bece93
Guido Lorenzetti <102630216+GuidoLorenzetti@users.noreply.github.com>
2022-06-17 01:20:59 +0000

    Updated to work with any city from: http://insideairbnb.com/get-the-data/

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # City-Price-Heat-Map
 Cost distribution of any city from the data provided by http://insideairbnb.com
+To use the program, go to http://insideairbnb.com/get-the-data/, and choose a city. Download the "listings.csv" file, and load it in the same folder as the console. Execute the program and you should see the heatmap for the city that you choose.
+
+Posibles fix if the map is not properly displayed:
+1- Try changing the "vmax" value in the 68 line.
+2- Make sure that the "listings.csv" file doesn´t have any extra character in it.
--- main.py
+++ main.py
@@ -7,33 +7,57 @@ def pos(r,l,lat,dist_hor):
   i = r
   while i < l:
     x += 1
-    if float(lat) > i and float(lat) < (i - dist_hor):
+    if float(lat) > i and float(lat) < (i + dist_hor):
       break
-    i -= dist_hor
+    i += dist_hor
   return x
 
-lat_izq = 55.733342
-lat_der = 55.614935
-long_arr = 12.423467
-long_ab = 12.686726
-inter = 50
+def lat_long(reader,inter):
+  cont = 0
+  for row in reader:
+    if row[0] == "id":
+          continue
+    else:
+      if cont == 0:
+        r = float(row[6])
+        l = float(row[6])
+        u = float(row[7])
+        d = float(row[7])
+      else:
+        if float(row[6]) < r:
+          r = float(row[6])
+        if float(row[6]) > l:
+          l = float(row[6])
+        if float(row[7]) < d:
+          d = float(row[7])
+        if float(row[7]) > u:
+          u = float(row[7])
+      cont += 1
+  dist_hor = abs((r - l) / inter)
+  dist_ver = abs((u - d) / inter)
+  return (r,l,u,d,dist_hor,dist_ver)
 
-dist_hor = (lat_der - lat_izq) / inter
-dist_ver = (long_arr - long_ab) / inter
+inter = 200
 
 cant = np.zeros((inter, inter))
 precio = np.zeros((inter, inter))
 precio_prom = np.zeros((inter, inter))
+cont = 0
+
+with open('listings.csv', newline='') as File:
+    reader = csv.reader(File)
+    r,l,d,u,dist_hor,dist_ver = lat_long(reader,inter)
 
 with open('listings.csv', newline='') as File:
     reader = csv.reader(File)
     for row in reader:
-        if row[0] == "id":
-          continue
-        x = pos (lat_der,lat_izq,row[6],dist_hor)
-        y = pos (long_arr,long_ab,row[7],dist_ver)
-        cant[((x-1),(y-1))] += 1
-        precio[((x-1),(y-1))] += float(row[9])
+      if row[0] == "id":
+        continue
+      x = pos (r,l,row[6],dist_hor)
+      y = pos (u,d,row[7],dist_ver)
+      cant[((x-1),(y-1))] += 1
+      precio[((x-1),(y-1))] += float(row[9])
+      cont += 1
 
 for l in range(inter):
   for m in range (inter):
@@ -42,6 +66,6 @@ for l in range(inter):
 
 figure, axes = plt.subplots()
 precio_prom = axes.pcolormesh(precio_prom, cmap='hot', vmin=0, vmax=2000)
-axes.set_title('Copenhagen Heat Map')
+axes.set_title('Heat Map')
 figure.colorbar(precio_prom)
 plt.show()
